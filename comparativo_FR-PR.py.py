dc1e54d8e241b3b74bac590d5c51847c58aeddf0
jefersonsantana580 <jefin.souza.92@gmail.com>
2026-04-13 22:47:27 -0300

    Update comparativo_FR-PR.py.py

--- comparativo_FR-PR.py.py
+++ comparativo_FR-PR.py.py
@@ -568,10 +568,8 @@ comp_p_merge = pd.merge(
     for m in meses:
         col_base = f"{m}_{base_name}"
         col_comp = f"{m}_{comp_name}"
-
     if col_base in comp_p_merge.columns:
             comp_p_merge[col_base] = comp_p_merge[col_base].fillna(0)
-
     if col_comp in comp_p_merge.columns:
             comp_p_merge[col_comp] = comp_p_merge[col_comp].fillna(0)
 
